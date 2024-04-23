2304d7f1ebbc0392d77672e539f13fcaf6e71cfe
OlegBalbek <154586492+OlegBalbek@users.noreply.github.com>
2024-04-23 14:59:00 +0300

    Add files via upload

--- dop/main.py
+++ dop/main.py
@@ -1,4 +1,5 @@
 import os, json
+import datetime
 files = []
 l = []
 files += os.listdir('transactions')
@@ -7,72 +8,40 @@ for i in files:
     l.append(json.loads(f.read()))
 a = l.copy()
 
-def first_task():
-    print("ПЕРВОЕ ЗАДАНИЕ")
-    from get_block.get_block_by_index_linear import get_block_by_index_linear
-    from get_block.get_block_by_idnex_binary import get_block_by_index_binary
-    first = a[0]  # Искомый элемент находится в начале
-    end = a[-1]  # Искомый элемент находится в конце
-    mid = a[len(a) // 2]  # Искомый элемент находится в центре
-    dvad = a[len(a) - 20]  # Искомый элемент находится на 20 месте с конца
-    temp = [first, end, mid, dvad]
-
-    print("ЛИНЕЙНЫЙ ПОИСК:")
-    for i in temp:
-        print(f"Линейный поиск индекса №{i['index']}: ", get_block_by_index_linear(a, i['index']), sep="\n", end="\n")
-
-    print("-" * 100)
-
-    print("ЛИНЕЙНЫЙ ПОИСК С ОТСОРТИРОВАННЫМ МАССИВОМ:")
-    for i in temp:
-        print(f"Линейный поиск индекса №{i['index']}: ", get_block_by_index_linear(sorted(a, key=lambda x: x['index']), i['index']), sep="\n", end="\n")
-
-    print("-" * 100)
-
-    print("БИНАРНЫЙ ПОИСК:")
-    for i in temp:
-        print(f"Бинарный поиск индекса №{i['index']}: ", get_block_by_index_binary(a, i['index']), sep="\n", end="\n")
-
-def second_task():
-    print("\nВТОРОЕ ЗАДАНИЕ")
-    from sort.bubble_sort import bubble_sort
-    from sort.selection_sort import selection_sort
-    print(f"НЕОТСОРТИРОВАННЫЙ МАССИВ:\n{a}")
-    print(f"СОРТИРОВКА ПУЗЫРЬКОМ:\n{bubble_sort(a)}")
-    print(f"СОРТИРОВКА ОТБОРОМ:\n{selection_sort(a)}")
-
-
 def third_task():
-    print("\nТРЕТЬЕ ЗАДАНИЕ")
-    from datetime import datetime, date
+    print("Агрегатные вычисления")
+
+    # Подсчет количества транзакций в каждом блоке и общего количества транзакций
     count_of_transactions = 0
-    ct = 0
-    third = sorted(a, key=lambda x: x['index'])
+    bolt = 0
+    three = sorted(a, key=lambda x: x['index'])
     miners = dict()
 
-    for i in third:
+    for i in three:
         count_of_transactions += len(i['transactions'])
-        ct += len(list(filter(lambda x: x['from'] != "SYSTEM" and i['index'] != 0, i['transactions'])))
+        bolt += len(list(filter(lambda x: x['from'] != "SYSTEM" and i['index'] != 0, i['transactions'])))
         print(f"Номер блока: {i['index']}. Количество транзакций (с учётом SYSTEM): {len(i['transactions'])}")
-    print(f"Общее количество транзакций: {count_of_transactions} (с учётом SYSTEM); {ct} (без учёта SYSTEM)\n")
+    print(f"Общее количество транзакций: {count_of_transactions} (с учётом SYSTEM); {bolt} (без учёта SYSTEM)\n")
 
+    # Нахождение самого низкого и самого высокого суммарного вознаграждения у майнеров
     list_of_values = []
     list_of_transactions = []
 
-    for i in third: # Суммарное количество вознаграждений
+    for i in three:
         if i["index"] == 0:
             continue
         list_of_values.append(i["transactions"][-1]["value"])
         if i['transactions'][-1]['to'] not in miners:
-            miners[i['transactions'][-1]['to']] = 0 # Добавление майнера в словарь
-        miners[i['transactions'][-1]['to']] += i['transactions'][-1]['value'] # Добавление награжденй
+            miners[i['transactions'][-1]['to']] = 0
+        miners[i['transactions'][-1]['to']] += i['transactions'][-1]['value']
     miners = sorted(miners.items(), key=lambda x: x[1])
     list_of_values = sorted(list_of_values)
 
     print(f"Самое низкое (суммарное) вознаграждение (у майнера): {miners[0]}\nСамое высокое (суммарное) вознаграждение (у майнера): {miners[-1]}\n")
     print(f"Самое низкое вознаграждение (в целом): {list_of_values[0]}\nСамое высокое вознаграждение (в целом): {list_of_values[-1]}\n")
 
-    for i in third:
+    # Подсчет среднего значения перевода в транзакциях (без учета награждений)
+    for i in three:
         if i["index"] == 0:
             continue
         for j in i["transactions"]:
@@ -81,19 +50,18 @@ def third_task():
             list_of_transactions.append(j["value"])
     print(f"Среднее значение перевода в транзакциях (награждения не учитываются): {sum(list_of_transactions)/len(list_of_transactions)}")
 
+    # Подсчет количества транзакций в каждую минуту
     minutes = {}
 
-    for i in third:
+    for i in three:
         minute = datetime.fromtimestamp(i['timestamp']).minute
         if minute not in minutes:
             minutes[minute] = 0
         else:
             minutes[minute] += 1
 
-    print("\nМИНУТЫ:")
+    print("\nМинуты:")
     for key, val in minutes.items():
         print(f"{key} м.: {val} зн.")
 
-first_task()
-second_task()
 third_task()
